efcc56da1f457c63b07f0c5001a47fa0497c1d7c
Corina Varas <corinacvp@gmail.com>
2020-11-25 13:48:23 -0300

    like for imagen only for user register

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Article, ImgWrapper, Img } from './styles'
-import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 import { FavButton } from '../FavButton/index'
 import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
@@ -8,10 +7,8 @@ import { Link } from '@reach/router'
 
 const DEFAULT_IMAGES = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGES }) => {
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGES }) => {
   const [show, element] = useNearScreen()
-  const key = `like${id}`
-  const [liked, setLiked] = useLocalStorage(key, false)
 
   return (
     <Article ref={element}>
@@ -27,11 +24,9 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGES }) => {
               {
                 (toggleLike) => {
                   const handleFavClick = () => {
-                    !liked && toggleLike({ variables: {
+                    toggleLike({ variables: {
                       input: { id }
                     } })
-
-                    setLiked(!liked)
                   }
                   return <FavButton onClick={handleFavClick} liked={liked} likes={likes} /> }
               }
--- src/pages/NoRegisteredUser.js
+++ src/pages/NoRegisteredUser.js
@@ -1,49 +1,61 @@
-import React from 'react'
-import Context from '../Context'
+import React, { Fragment, useContext } from 'react'
+import { Context } from '../Context'
 import { UserForm } from '../components/UserForm/index'
 import { RegisterMutation } from '../container/RegisterMutation'
 import { LoginMutation } from '../container/LoginMutation'
 
-const login = 'Inicia sesión con tu cuenta petslovers y descubre el increible mundo de las mascotas'
+export const NoRegisteredUser = () => {
+  const { activateAuth } = useContext(Context)
 
-export const NoRegisteredUser = () => ( 
-  <Context.Consumer>
-    {
-      ({ activateAuth }) => {
-        return (
-          <>
-            <RegisterMutation>
-              {
-                (register, { data, loading, error }) => {
-                  const onSubmit = ({ email, password }) => {
-                    const input = { email, password }
-                    const variables = { input }
-                    register({ variables }).then(activateAuth)
-                  }
+  return ( 
+  <Fragment>
+      <RegisterMutation>
+        {(register, { data, loading, error }) => {
+          const onSubmit = ({ email, password }) => {
+            const input = { email, password };
+            const variables = { input };
+            register({ variables }).then(({ data }) => {
+              const { signup } = data;
+              activateAuth(signup);
+            });
+          };
 
-                  const errorMsg = error && 'Usuario ya existe'
+          const errorMsg = error && 'Usuario ya existe';
+          const title = '¿No tienes una cuenta? Registrate'
 
-                  return <UserForm disabled={loading} error={errorMsg} title='¿No tienes una cuenta? Registrate' onSubmit={onSubmit} />
-                }
-              }
-            </RegisterMutation>
-            <LoginMutation>
-              {
-                (login, { data, loading, error }) => {
-                  const onSubmit = ({ email, password }) => {
-                    const input = { email, password }
-                    const variables = { input }
-                    login({ variables }).then(activateAuth)
-                  }
+          return (
+            <UserForm
+              disabled={loading}
+              error={errorMsg}
+              title={title}
+              onSubmit={onSubmit}
+            />
+          );
+        }}
+      </RegisterMutation>
+      <LoginMutation>
+        {(login, { data, loading, error }) => {
+          const onSubmit = ({ email, password }) => {
+            const input = { email, password };
+            const variables = { input };
+            login({ variables }).then(({ data }) => {
+              const { login } = data;
+              activateAuth(login);
+            });
+          };
+          const errorMsg = error && 'Usuario o contraseña incorrectos';
+          const title ='Inicia sesión con tu cuenta petslovers y descubre el increible mundo de las mascotas'
 
-                  const errorMsg = error && 'Usuario o contraseña incorrectos'
-                  return <UserForm disabled={loading} error={errorMsg} title={login} onSubmit={onSubmit} />
-                }
-              }
-            </LoginMutation>
-          </>
-        )
-      }
-    }
-  </Context.Consumer>
-)
+          return (
+            <UserForm
+              disabled={loading}
+              error={errorMsg}
+              title={title}
+              onSubmit={onSubmit}
+            />
+          )
+        }}
+      </LoginMutation>
+    </Fragment>
+  )
+}
