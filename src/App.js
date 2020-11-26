76a1eda2cc368779a94c8c6e6675dc8d4245b822
Corina Varas <corinacvp@gmail.com>
2020-11-25 23:16:20 -0300

    Add react lazy, component suspense and proptypes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React, { useContext } from "react";
+import React, { useContext, Suspense } from "react";
 import { GlobalStyles } from "./styles/GlobalStyles";
 import { Logo } from "./components/Logo";
 import { NavBar } from "./components/NavBar";
 import { Home } from "./pages/Home";
 import { Detail } from "./pages/Detail";
-import { Favs } from "./pages/Favs";
 import { User } from "./pages/User";
 import { NoRegisteredUser } from "./pages/NoRegisteredUser";
 import { NotFound } from './pages/NotFound'
 import { Redirect, Router } from "@reach/router";
 import { Context } from "./Context";
 
+const Favs = React.lazy(() => import('./pages/Favs'))
+
 export const App = () => {
   const { isAuth } = useContext(Context);
   return (
-    <>
+    <Suspense fallback={<div />}>
       <GlobalStyles />
       <Logo />
       <Router>
@@ -30,6 +31,6 @@ export const App = () => {
         <User path="/user" />
       </Router>
       <NavBar />
-    </>
+    </Suspense>
   );
 };
